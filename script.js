0ec75f98f364271b6b9924a5d2f6b56c4c7b523d
Solarpunkie <solarpunkie@outlook.com>
2024-07-10 21:59:46 -0300

    up

---
+++ script.js
@@ -0,0 +1,26 @@
+// // Function to shuffle the SoundCloud playlist
+// function shufflePlaylist() {
+//     // Use the SoundCloud widget API to control the player
+//     var widget = SC.Widget(document.getElementById('soundcloud-player'));
+
+//     // Get the total number of tracks in the playlist
+//     widget.getSounds(function (sounds) {
+//         // Generate a random index
+//         var randomIndex = Math.floor(Math.random() * sounds.length);
+
+//         // Play the track at the random index
+//         widget.skip(randomIndex);
+//     });
+// }
+
+// // Function to play the next track in the SoundCloud playlist
+// function nextTrack() {
+//     // Use the SoundCloud widget API to control the player
+//     var widget = SC.Widget(document.getElementById('soundcloud-player'));
+
+//     // Play the next track
+//     widget.next();
+// }
+
+
+
